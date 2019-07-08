73ffbbf037a9f55dc19ca9ae73db6836805293e1
shaik sha <mr.shaiksha@gmail.com>
2019-07-08 09:49:09 +0530

    added switches

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter as Router, Route} from 'react-router-dom'; 
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'; 
 import Navigation from '../Components/Navigation/Navigation';
 import Home from '../Components/Home/Home';
 import Tips from '../Components/Tips/Tips';
@@ -10,10 +10,12 @@ function App() {
   return (
     <div>
       <Router>
-        <Route path="/" component={Navigation}/>
-        <Route path="/" exact component={Home} />
-        <Route path="/tips" component={Tips} />
-        <Route path="/about" component={About} />
+          <Route path="/" component={Navigation}/>
+          <Route path="/" exact component={Home} />
+        <Switch>
+          <Route path="/tips" component={Tips} />
+          <Route path="/about" component={About} />
+        </Switch>
       </Router>
     </div>
   );
